4f7e36ef27c171a159f839e4b07bf2bc58f85a90
Justin Chapman <commonmodestudio@gmail.com>
2025-06-03 23:01:27 -0400

    net

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,26 +1,42 @@
 #![no_std]
 #![no_main]
 
-use core::sync::atomic::{AtomicU32, AtomicU8, Ordering};
-use defmt::{debug, error, info, Format};
+use core::net::{Ipv4Addr, SocketAddrV4};
+use core::{
+    str::FromStr,
+    sync::atomic::{AtomicU32, AtomicU8, Ordering},
+};
+use defmt::{debug, error, info, warn, Format};
+use edge_dhcp::{
+    io::{self, DEFAULT_SERVER_PORT},
+    server::{Server, ServerOptions},
+};
+use edge_nal::UdpBind;
+use edge_nal_embassy::{Udp, UdpBuffers};
 use embassy_executor::Spawner;
+use embassy_net::{tcp::TcpSocket, IpListenEndpoint, Ipv4Cidr, Runner, Stack, StaticConfigV4};
 use embassy_time::{Duration, Instant, Timer};
+use embedded_io_async::Write;
 use esp_hal::{
     clock::CpuClock,
-    gpio::{Level, Output, OutputConfig},
+    gpio::{Input, InputConfig, Level, Output, OutputConfig, Pull},
     i2c::master::{Config, I2c},
     time::Rate,
     timer::{systimer::SystemTimer, timg::TimerGroup},
     Blocking,
 };
+use esp_wifi::wifi::{
+    AccessPointConfiguration, Configuration, WifiController, WifiDevice, WifiEvent, WifiState,
+};
 use mfrc522::{comm::blocking::i2c::I2cInterface, GenericUid, Initialized, Mfrc522, Uid};
+use static_cell::StaticCell;
 extern crate alloc;
 
 /// marker colors, with map to and from their rfid uid values
 /// explicitly map to/from u8 values (which is what is persisted in global state via AtomicU8)
 #[repr(u8)]
 #[derive(Format, PartialEq, Clone)]
-pub enum MarkerColor {
+enum MarkerColor {
     Red = 1,
     Brown = 2,
     BlueLagoon = 3,
@@ -35,7 +51,7 @@ pub enum MarkerColor {
     Violet = 12,
 }
 impl MarkerColor {
-    pub fn uid(&self) -> [u8; 7] {
+    fn uid(&self) -> [u8; 7] {
         match self {
             MarkerColor::Red => [4, 61, 60, 18, 54, 30, 145],
             MarkerColor::Brown => [4, 61, 59, 18, 54, 30, 145],
@@ -51,7 +67,7 @@ impl MarkerColor {
             MarkerColor::Violet => [4, 61, 49, 18, 54, 30, 145],
         }
     }
-    pub fn from_uid(generic_uid: &GenericUid<7>) -> Option<Self> {
+    fn from_uid(generic_uid: &GenericUid<7>) -> Option<Self> {
         let uid = generic_uid.as_bytes();
         if uid == MarkerColor::Red.uid() {
             Some(MarkerColor::Red)
@@ -106,10 +122,9 @@ impl TryFrom<u8> for MarkerColor {
 // global state
 
 /// last time marker color was updated - used for led flash
-pub static LAST_MARKER_COLOR_UPDATED_AT: AtomicU32 =
-    AtomicU32::new(Instant::MIN.as_millis() as u32);
+static LAST_MARKER_COLOR_UPDATED_AT: AtomicU32 = AtomicU32::new(Instant::MIN.as_millis() as u32);
 /// last detected marker color
-pub static LAST_MARKER_COLOR: AtomicU8 = AtomicU8::new(0);
+static LAST_MARKER_COLOR: AtomicU8 = AtomicU8::new(0);
 
 #[panic_handler]
 fn panic(_: &core::panic::PanicInfo) -> ! {
@@ -120,28 +135,48 @@ fn panic(_: &core::panic::PanicInfo) -> ! {
 async fn main(spawner: Spawner) {
     // real time transfer - for debug/logging
     rtt_target::rtt_init_defmt!();
-
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
-
     esp_alloc::heap_allocator!(size: 72 * 1024);
-
     let timer0 = SystemTimer::new(peripherals.SYSTIMER);
     esp_hal_embassy::init(timer0.alarm0);
-
     info!("embassy initialized");
 
+    // initialize wifi
+    // https://github.com/esp-rs/esp-hal/blob/main/examples/src/bin/wifi_embassy_access_point.rs
+    // https://tasmota.github.io/docs/Commands/#with-web-requests
+    static WIFI_INIT: StaticCell<esp_wifi::EspWifiController> = StaticCell::new();
+    static RESOURCES: StaticCell<embassy_net::StackResources<3>> = StaticCell::new();
     let timer1 = TimerGroup::new(peripherals.TIMG0);
-    let _init = esp_wifi::init(
-        timer1.timer0,
-        esp_hal::rng::Rng::new(peripherals.RNG),
-        peripherals.RADIO_CLK,
-    )
-    .unwrap();
+    let mut rng = esp_hal::rng::Rng::new(peripherals.RNG);
+    let seed = rng.random().into();
+    let init = WIFI_INIT.init(esp_wifi::init(timer1.timer0, rng, peripherals.RADIO_CLK).unwrap());
+    let (ctrl, interfaces) = esp_wifi::wifi::new(init, peripherals.WIFI).unwrap();
+    let device = interfaces.ap;
+    let gw_ip_addr_str = "192.168.2.1";
+    let gw_ip_addr = Ipv4Addr::from_str(gw_ip_addr_str).expect("failed to parse gateway ip");
+    let config = embassy_net::Config::ipv4_static(StaticConfigV4 {
+        address: Ipv4Cidr::new(gw_ip_addr, 24),
+        gateway: Some(gw_ip_addr),
+        dns_servers: Default::default(),
+    });
+    let (stack, runner) = embassy_net::new(
+        device,
+        config,
+        RESOURCES.init(embassy_net::StackResources::new()),
+        seed,
+    );
+    info!("wifi controller initialized");
 
     // initialize led
     let mut led: Output<'_> = Output::new(peripherals.GPIO7, Level::Low, OutputConfig::default());
     led.set_low();
+
+    // initialize button
+    let button = Input::new(
+        peripherals.GPIO9,
+        InputConfig::default().with_pull(Pull::Up),
+    );
 
     // rfid reader
     let sda = peripherals.GPIO2;
@@ -195,15 +230,35 @@ async fn main(spawner: Spawner) {
         }
     }
 
+    // start up tasks
     spawner.spawn(rfid_task(mfrc522)).unwrap();
     spawner.spawn(led_task(led)).unwrap();
+    spawner.spawn(button_task(button)).unwrap();
+    spawner.spawn(connection_task(ctrl)).unwrap();
+    spawner.spawn(net_task(runner)).unwrap();
+    spawner.spawn(run_dhcp(stack, gw_ip_addr_str)).unwrap();
+    spawner.spawn(listener_task(stack)).unwrap();
 }
 
 // tasks
 
+/// task for button press
+#[embassy_executor::task]
+async fn button_task(button: Input<'static>) {
+    let mut was_pressed = false;
+    loop {
+        let is_pressed = button.is_low();
+        if is_pressed && !was_pressed {
+            reset_marker_color();
+        }
+        was_pressed = is_pressed;
+        Timer::after(Duration::from_millis(100)).await;
+    }
+}
+
 /// loop for managing led
 #[embassy_executor::task]
-pub async fn led_task(mut led: Output<'static>) {
+async fn led_task(mut led: Output<'static>) {
     loop {
         let now = Instant::now().as_millis() as u32;
         let last_uid_at = LAST_MARKER_COLOR_UPDATED_AT.load(Ordering::Relaxed);
@@ -218,7 +273,7 @@ pub async fn led_task(mut led: Output<'static>) {
 
 /// loop for rfid reader
 #[embassy_executor::task]
-pub async fn rfid_task(mut mfrc522: Mfrc522<I2cInterface<I2c<'static, Blocking>>, Initialized>) {
+async fn rfid_task(mut mfrc522: Mfrc522<I2cInterface<I2c<'static, Blocking>>, Initialized>) {
     loop {
         if let Ok(atqa) = mfrc522.new_card_present() {
             match mfrc522.select(&atqa) {
@@ -250,6 +305,137 @@ pub async fn rfid_task(mut mfrc522: Mfrc522<I2cInterface<I2c<'static, Blocking>>
     }
 }
 
+#[embassy_executor::task]
+async fn net_task(mut runner: Runner<'static, WifiDevice<'static>>) {
+    info!("starting net task");
+    runner.run().await
+}
+
+#[embassy_executor::task]
+async fn connection_task(mut controller: WifiController<'static>) {
+    loop {
+        if let WifiState::ApStarted = esp_wifi::wifi::wifi_state() {
+            controller.wait_for_event(WifiEvent::ApStop).await;
+            Timer::after(Duration::from_millis(5000)).await
+        }
+        if !matches!(controller.is_started(), Ok(true)) {
+            let client_config = Configuration::AccessPoint(AccessPointConfiguration {
+                ssid: "magic-markers".try_into().unwrap(),
+                ..Default::default()
+            });
+            controller.set_configuration(&client_config).unwrap();
+            info!("starting wifi...");
+            controller.start_async().await.unwrap();
+            info!("wifi started");
+        }
+    }
+}
+
+#[embassy_executor::task]
+async fn listener_task(stack: Stack<'static>) {
+    info!("starting listener task");
+    let mut rx_buffer = [0; 1536];
+    let mut tx_buffer = [0; 1536];
+    loop {
+        if stack.is_link_up() {
+            break;
+        }
+        Timer::after(Duration::from_millis(500)).await;
+    }
+    info!("connect to the `magic-markers` network and visit http://192.168.2.1:8080");
+    while !stack.is_config_up() {
+        Timer::after(Duration::from_millis(100)).await
+    }
+    let mut socket = TcpSocket::new(stack, &mut rx_buffer, &mut tx_buffer);
+    socket.set_timeout(Some(embassy_time::Duration::from_secs(10)));
+    loop {
+        let r = socket
+            .accept(IpListenEndpoint {
+                addr: None,
+                port: 8080,
+            })
+            .await;
+        info!("http request");
+        if r.is_err() {
+            info!("connect error");
+            continue;
+        }
+        let mut buffer = [0u8; 1024];
+        let mut pos = 0;
+        loop {
+            match socket.read(&mut buffer).await {
+                Ok(0) => {
+                    info!("read eof");
+                    break;
+                }
+                Ok(len) => {
+                    let to_print =
+                        unsafe { core::str::from_utf8_unchecked(&buffer[..(pos + len)]) };
+                    if to_print.contains("\r\n\r\n") {
+                        break;
+                    }
+                    pos += len;
+                }
+                Err(_) => {
+                    info!("read error");
+                    break;
+                }
+            };
+        }
+        let r = socket
+            .write_all(
+                b"HTTP/1.0 200 OK\r\n\r\n\
+            <html>\
+                <body>\
+                    <h1>magic-markers!</h1>\
+                </body>\
+            </html>\r\n\
+            ",
+            )
+            .await;
+        if r.is_err() {
+            info!("write error");
+        }
+
+        let r = socket.flush().await;
+        if r.is_err() {
+            info!("flush error");
+        }
+        Timer::after(Duration::from_millis(1000)).await;
+        socket.close();
+        Timer::after(Duration::from_millis(1000)).await;
+        socket.abort();
+    }
+}
+
+#[embassy_executor::task]
+async fn run_dhcp(stack: Stack<'static>, gw_ip_addr: &'static str) {
+    info!("starting dhcp task");
+    let ip = Ipv4Addr::from_str(gw_ip_addr).expect("dhcp task failed to parse gw ip");
+    let mut buf = [0u8; 1500];
+    let mut gw_buf = [Ipv4Addr::UNSPECIFIED];
+    let buffers = UdpBuffers::<3, 1024, 1024, 10>::new();
+    let unbound_socket = Udp::new(stack, &buffers);
+    let mut bound_socket = unbound_socket
+        .bind(core::net::SocketAddr::V4(SocketAddrV4::new(
+            Ipv4Addr::UNSPECIFIED,
+            DEFAULT_SERVER_PORT,
+        )))
+        .await
+        .unwrap();
+    loop {
+        _ = io::server::run(
+            &mut Server::<_, 64>::new_with_et(ip),
+            &ServerOptions::new(ip, Some(&mut gw_buf)),
+            &mut bound_socket,
+            &mut buf,
+        )
+        .await
+        .inspect_err(|_| warn!("dhcp server error"));
+        Timer::after(Duration::from_millis(500)).await;
+    }
+}
+
 // helpers
 
 /// when we detect a marker from our set of markers, update global state
@@ -266,4 +452,10 @@ fn marker_detected(color: MarkerColor) {
     LAST_MARKER_COLOR.store(color.clone() as u8, Ordering::Relaxed);
     LAST_MARKER_COLOR_UPDATED_AT.store(Instant::now().as_millis() as u32, Ordering::Relaxed);
     info!("color update: {:?}", color);
+}
+
+fn reset_marker_color() {
+    LAST_MARKER_COLOR.store(0, Ordering::Relaxed);
+    LAST_MARKER_COLOR_UPDATED_AT.store(Instant::now().as_millis() as u32, Ordering::Relaxed);
+    info!("marker color reset");
 }
